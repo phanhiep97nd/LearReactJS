0469e9f5850c97ada0f710d53382d09e8c2c789b
phanvanhiep <phanvanhiep@luvina.net>
2024-08-13 09:13:49 +0700

    learn typescript

---
+++ BaiTapThucHanh/TypeScript/my-first-typescript-app/index.js
@@ -0,0 +1,11 @@
+"use strict";
+class People {
+    constructor(name, age, address) {
+        this.showInfo = () => console.log(`Hello ${this.name} , age: ${this.age}, address: ${this.address}!`);
+        this.name = name;
+        this.age = age;
+        //this.address = address;
+    }
+}
+let people = new People('HiepPV', 27, 'NamDinh');
+people.showInfo();
---
+++ BaiTapThucHanh/TypeScript/my-first-typescript-app/index.ts
@@ -0,0 +1,16 @@
+class People {
+    name: string;
+    age: number;
+    address?: string | number;
+
+    constructor(name: string, age: number, address: string | number) {
+        this.name = name;
+        this.age = age;
+        //this.address = address;
+    }
+
+    showInfo = () => console.log(`Hello ${this.name} , age: ${this.age}, address: ${this.address}!`);
+}
+
+let people = new People('HiepPV', 27, 'NamDinh');
+people.showInfo();
