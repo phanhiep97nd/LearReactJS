bd03f1ed8306df8b3c3b0934f6b69caafcb25caf
phanvanhiep <phanvanhiep@luvina.net>
2024-08-30 17:46:44 +0700

    update

--- BaiTapThucHanh/react-js-first-app/src/components/base/Header.jsx
+++ BaiTapThucHanh/react-js-first-app/src/components/base/Header.jsx
@@ -3,60 +3,60 @@ import '../../sass/header.scss';
 
 const Header = () => {
 	return (
-		<header class="header" data-header>
-			<div class="container">
+		<header className="header" data-header>
+			<div className="container">
 
-				<div class="overlay" data-overlay></div>
+				<div className="overlay" data-overlay></div>
 
-				<a href="#home" class="logo" id="home">
+				<a href="#home" className="logo" id="home">
 					<h2 style={{ color: "gray" }}>Mr.Phóng</h2>
 
 				</a>
 
-				<nav class="navbar" data-navbar>
-					<ul class="navbar-list">
+				<nav className="navbar" data-navbar>
+					<ul className="navbar-list">
 
 					<li>
-						<a href="#home" class="navbar-link" data-nav-link>Home</a>
+						<a href="#home" className="navbar-link" data-nav-link>Home</a>
 					</li>
 
 					<li>
-						<a href="#featured-car" class="navbar-link" data-nav-link>Đặt xe</a>
+						<a href="#featured-car" className="navbar-link" data-nav-link>Đặt xe</a>
 					</li>
 
 					<li>
-						<a href="#contact" class="navbar-link" data-nav-link>Liên hệ</a>
+						<a href="#contact" className="navbar-link" data-nav-link>Liên hệ</a>
 					</li>
 
 					<li>
-						<a href="#blog" class="navbar-link" data-nav-link>Về chúng tôi</a>
+						<a href="#blog" className="navbar-link" data-nav-link>Về chúng tôi</a>
 					</li>
 
 					</ul>
 				</nav>
 
-				<div class="header-actions">
+				<div className="header-actions">
 
-					<div class="header-contact">
-					<a href="tel:0868266815" class="contact-link">0868 266 815</a>
+					<div className="header-contact">
+                        <a href="tel:0868266815" className="contact-link">0868 266 815</a>
 
-					<span class="contact-time">Phục vụ 24/7</span>
+                        <span className="contact-time">Phục vụ 24/7</span>
 					</div>
 
-					<a href="#featured-car" class="btn" aria-labelledby="aria-label-txt">
-					<ion-icon name="car-outline"></ion-icon>
+					<a href="#featured-car" className="btn" aria-labelledby="aria-label-txt">
+                        <ion-icon name="car-outline"></ion-icon>
 
-					<span id="aria-label-txt">Đặt xe</span>
+                        <span id="aria-label-txt">Đặt xe</span>
 					</a>
 
-					<a href="#contact" class="btn user-btn" aria-label="Profile">
+					<a href="#contact" className="btn user-btn" aria-label="Profile">
 					<ion-icon name="person-outline"></ion-icon>
 					</a>
 
-					<button class="nav-toggle-btn" data-nav-toggle-btn aria-label="Toggle Menu">
-					<span class="one"></span>
-					<span class="two"></span>
-					<span class="three"></span>
+					<button className="nav-toggle-btn" data-nav-toggle-btn aria-label="Toggle Menu">
+                        <span className="one"></span>
+                        <span className="two"></span>
+                        <span className="three"></span>
 					</button>
 
 				</div>
